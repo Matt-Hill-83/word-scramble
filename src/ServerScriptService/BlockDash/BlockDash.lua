2c051b24f327ab273f981ab92f41c1e1d658260f
Matt Hill <matt.hill.email@gmail.com>
2021-01-16 03:36:45 -0800

    works

--- src/ServerScriptService/BlockDash/BlockDash.lua
+++ src/ServerScriptService/BlockDash/BlockDash.lua
@@ -19,8 +19,8 @@ local allLetters = {
 local numRow = 26
 local numCol = 26
 
-local words = {"CAT", "HAT"}
--- local words = {"CAT", "HAT", "MAT", "PAT", "SAT", "BOG", "RAT"}
+-- local words = {"CAT", "HAT"}
+local words = {"CAT", "HAT", "MAT", "PAT", "SAT", "BOG", "RAT"}
 -- local words = {
 --     "CAT", "HAT", "MAT", "PAT", "SAT", "BOG", "VAN", "RAN", "CAN", "AN", "PAN",
 --     "DAN", "FAN", "BAN", "TAN"
