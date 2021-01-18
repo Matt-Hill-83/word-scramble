e8a9d2de8b6f3f169cce162911a601333a50b2cb
Matt Hill <matt.hill.email@gmail.com>
2021-01-17 17:25:15 -0800

    works

--- src/ServerScriptService/BlockDash/ChestScript/init.lua
+++ src/ServerScriptService/BlockDash/ChestScript/init.lua
@@ -9,20 +9,13 @@ local function init(chest)
     print('chestHitBox' .. ' - start');
     print(chestHitBox);
 
-    script.Parent = chest
-    print('CHEST SCRPT');
-    print('CHEST SCRPT');
-    print('CHEST SCRPT');
-    print('CHEST SCRPT');
-    print('CHEST SCRPT');
-
-    -- script.ChestLocalScript.Chest.Value = script.Parent -- setting a value
+    -- chest = chest
 
     local reward
-    if #script.Parent.Reward:GetChildren() > 0 then
-        local originalReward = script.Parent.Reward:GetChildren()[1]
+    if #chest.Reward:GetChildren() > 0 then
+        local originalReward = chest.Reward:GetChildren()[1]
         reward = originalReward:Clone()
-        local newModel = Instance.new("Model", script.Parent.Reward)
+        local newModel = Instance.new("Model", chest.Reward)
         for _, child in pairs(originalReward:GetChildren()) do
             if not (child:IsA("Script") or child:IsA("LocalScript")) then
                 if child:IsA("BasePart") then
@@ -36,59 +29,16 @@ local function init(chest)
     end
 
     function onChestTouched(obj)
-        print('onChestTouched');
-        print('onChestTouched');
-        print('onChestTouched');
-        print('onChestTouched');
         local player = game.Players:GetPlayerFromCharacter(obj.Parent)
         if player then OnLidToggle(player) end
     end
 
-    -- local chestHitBox = Instance.new("Part", script.Parent)
-    -- chestHitBox.Size = Vector3.new(5, 30, 4)
-    -- chestHitBox.Anchored = true
-    -- chestHitBox.CanCollide = false
-    -- chestHitBox.Transparency = 0
-    -- chestHitBox.CFrame = (script.Parent.PrimaryPart.CFrame +
-    --                          Vector3.new(0, 1.5, 0)) *
-    --                          CFrame.Angles(0, math.pi / 2, 0)
+    -- chestHitBox.Touched:connect(onChestTouched)
 
-    chestHitBox.Touched:connect(onChestTouched)
-
-    -- game.Players.PlayerAdded:connect(function(player) -- when a player si added
-    --     player.CharacterAdded:connect(
-    --         function() -- and whenever their character is loaded then
-    --             if true then -- if this is not supposed to be a static object then
-    --                 -- if script.Parent.Configurations.StaticObject.Value == false then -- if this is not supposed to be a static object then
-    --                 local newScript = script.ChestLocalScript:Clone() -- clone the local script
-    --                 newScript.Parent = player.PlayerGui -- and put it in the player's playerGui
-    --             end
-    --         end)
-    -- end)
-
-    -- local hinge = script.Parent.Lid.HingePart -- making it easier to access this part
-    -- local lidOpen = false -- so we know if the chest is open or closed
-
-    if true then -- checking if this chest is supposed to open
-        -- if script.Parent.Configurations.StaticObject.Value == false then -- checking if this chest is supposed to open
-        -- hinge.BodyPosition.position = Vector3.new(hinge.Position.X,
-        --                                           hinge.Position.Y,
-        --                                           hinge.Position.Z)
-        -- hinge.BodyGyro.cframe = hinge.CFrame
-        -- for i, v in pairs(script.Parent.Lid:GetChildren()) do
-        --     v.Anchored = false
-        -- end
-    else
-        -- script.Parent.Lid.Lock.ClickDetector.MaxActivationDistance = 20
-    end
-
-    function OnLidToggle(player)
-        -- if lidOpen == false then
-        --     hinge.BodyGyro.cframe = hinge.BodyGyro.cframe *
-        --                                 CFrame.Angles(0, 0, math.rad(-90))
-        --     lidOpen = true
-        -- end
-
+    function OnLidToggle(obj)
+        local player = game.Players:GetPlayerFromCharacter(obj.Parent)
+        if not player then return end
+        -- function OnLidToggle(player)
         if not player.Backpack:FindFirstChild(reward.Name) and player.Character and
             not player.Character:FindFirstChild(reward.Name) then
             local toGive = reward:Clone()
@@ -101,17 +51,15 @@ local function init(chest)
             if not game.StarterPack:FindFirstChild(reward.Name) then
                 toGive:Clone().Parent = game.StarterPack
             end
-            -- toGive.Parent = player.Backpack
             player.Character.Humanoid:UnequipTools()
-            -- player.Character.Humanoid:EquipTool(toGive)
             toGive.Parent = player.Backpack
             player.Character.Humanoid:EquipTool(toGive)
-            -- toGive.Parent = player.Character
         end
 
     end
 
-    -- script.Parent.LidToggle.OnServerEvent:connect(OnLidToggle)
+    chestHitBox.Touched:connect(OnLidToggle)
+
 end
 
 module.init = init
