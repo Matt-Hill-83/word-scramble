edf42b25975c2cc8d2fe49e7c1d5bca8b3308936
Matt Hill <matt.hill.email@gmail.com>
2021-01-17 18:59:03 -0800

    about to refactor

--- src/ServerScriptService/LetterGrabber/LetterGrabber.lua
+++ src/ServerScriptService/LetterGrabber/LetterGrabber.lua
@@ -1,7 +1,7 @@
 local Sss = game:GetService("ServerScriptService")
 local CS = game:GetService("CollectionService")
 local LetterFallUtils = require(Sss.Source.LetterFall.LetterFallUtils)
-local PlayerStatManager = require(Sss.Source.AddRemoteObjects.PlayerStatManager)
+-- local PlayerStatManager = require(Sss.Source.AddRemoteObjects.PlayerStatManager)
 
 local Utils = require(Sss.Source.Utils.U001GeneralUtils)
 local Utils3 = require(Sss.Source.Utils.U003PartsUtils)
