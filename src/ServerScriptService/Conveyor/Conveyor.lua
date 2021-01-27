7e35018b695e0d21a06bb83dbb99c0ef6519db11
Matt Hill <matt.hill.email@gmail.com>
2021-01-27 01:41:43 -0800

    sasef

--- src/ServerScriptService/Conveyor/Conveyor.lua
+++ src/ServerScriptService/Conveyor/Conveyor.lua
@@ -8,37 +8,38 @@ local module = {}
 
 local function initbeltPlate(props)
     local beltPlateIndex = props.beltPlateIndex
-    local sectorFolder = props.sectorFolder
-    local beltPlateTemplate = props.beltPlateTemplate
     local miniGameState = props.miniGameState
 
     local numRow = miniGameState.numRow
     local numCol = miniGameState.numCol
     local letterSpacingFactor = miniGameState.letterSpacingFactor
     local rackLetterSize = miniGameState.rackLetterSize
+    local sectorFolder = miniGameState.sectorFolder
 
     local conveyor = Utils.getFirstDescendantByName(sectorFolder, "Conveyor")
     local stopPlate = Utils.getFirstDescendantByName(conveyor, "Stop")
+    local beltPlateTemplate = Utils.getFirstDescendantByName(conveyor,
+                                                             "BeltPlateTemplate")
 
     local newBeltPlate = beltPlateTemplate:Clone()
     newBeltPlate.Parent = conveyor
     newBeltPlate.Name = 'NewBeltPlate'
     local belt = newBeltPlate.Belt
-    belt.BeltWeld.Enabled = false
 
+    belt.BeltWeld.Enabled = false
     local sizeX = numCol * rackLetterSize * letterSpacingFactor
     local sizeZ = numRow * rackLetterSize * letterSpacingFactor
     belt.Size = Vector3.new(sizeX, 1, sizeZ)
 
-    local position = Vector3.new(-belt.Size.X * beltPlateIndex * 1.05 * 1, 0, 0)
+    local position = Vector3.new(-belt.Size.X * beltPlateIndex * 1.05, 0, 0)
 
     belt.CFrame = Utils3.setCFrameFromDesiredEdgeOffset(
                       {
             parent = stopPlate,
             child = belt,
             offsetConfig = {
-                useParentNearEdge = Vector3.new(1, 1, 0),
-                useChildNearEdge = Vector3.new(-1, -1, 0),
+                useParentNearEdge = Vector3.new(-1, 1, 0),
+                useChildNearEdge = Vector3.new(1, -1, 0),
                 offsetAdder = position
             }
         })
@@ -81,8 +82,8 @@ local function initbeltPlate(props)
                             useParentNearEdge = Vector3.new(1, 0, 0),
                             useChildNearEdge = Vector3.new(1, 0, 0),
                             offsetAdder = Vector3.new(
-                                -belt.Size.X * miniGameState.numBelts * 1.05 * 1,
-                                0, 0)
+                                -belt.Size.X * miniGameState.numBelts * 1.05, 0,
+                                0)
                         }
                     })
             end
@@ -99,24 +100,20 @@ local function initConveyors(miniGameState)
     local initComplete = false
 
     local conveyor = Utils.getFirstDescendantByName(sectorFolder, "Conveyor")
-
     local numBelts = miniGameState.numBelts
     local glassTop = Utils.getFirstDescendantByName(conveyor, "GlassTop")
-
-    local beltPlateTemplate = Utils.getFirstDescendantByName(conveyor,
-                                                             "BeltPlateTemplate")
 
     local function config()
         for beltPlateIndex = 1, numBelts do
             local beltPlateProps = {
-                numBelts = numBelts,
                 beltPlateIndex = beltPlateIndex,
-                sectorFolder = sectorFolder,
-                beltPlateTemplate = beltPlateTemplate,
                 miniGameState = miniGameState
             }
-            module.initbeltPlate(beltPlateProps)
+            initbeltPlate(beltPlateProps)
         end
+
+        local beltPlateTemplate = Utils.getFirstDescendantByName(conveyor,
+                                                                 "BeltPlateTemplate")
         beltPlateTemplate:Destroy()
     end
     config()
@@ -133,16 +130,13 @@ local function initConveyors(miniGameState)
                 -- end
                 local beltPlates = Utils.getDescendantsByName(conveyor,
                                                               "NewBeltPlate")
-                print('beltPlates' .. ' - start');
-                print(beltPlates);
-                -- Populate each belt plate with a complete grid with all words
-                for plateIndex, beltPlate in ipairs(beltPlates) do
 
+                -- Populate each belt plate with a complete grid with all words
+                for _, beltPlate in ipairs(beltPlates) do
                     local belt = beltPlate.Belt
                     belt.BeltWeld.Enabled = false
 
                 end
-                beltPlateTemplate:Destroy()
             end
         end
     end
@@ -151,5 +145,4 @@ local function initConveyors(miniGameState)
 end
 
 module.initConveyors = initConveyors
-module.initbeltPlate = initbeltPlate
 return module
