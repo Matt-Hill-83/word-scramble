e8a7268c22f1a6dad84872cfb71c4dbf33fcd8e4
Matt Hill <matt.hill.email@gmail.com>
2021-01-30 02:55:11 -0800

    asdf

--- src/ServerScriptService/AddRemoteObjects/AddToBase.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase.lua
@@ -1,5 +1,6 @@
 local module = {}
 local Sss = game:GetService("ServerScriptService")
+local CS = game:GetService("CollectionService")
 
 local Utils = require(Sss.Source.Utils.U001GeneralUtils)
 local Utils3 = require(Sss.Source.Utils.U003PartsUtils)
@@ -75,10 +76,11 @@ local function addRemoteObjects()
     local mainSpawn = Utils.getFirstDescendantByName(myStuff, "MainSpawn")
     Entrance.initEntrance(workspace)
 
-    local islandPositionerFolder = Utils.getFirstDescendantByName(blockDash,
-                                                                  "IslandPositioners")
+    local islandPositioners = CS:GetTagged("IslandPositioner")
+    -- local islandPositionerFolder = Utils.getFirstDescendantByName(blockDash,
+    --                                                               "IslandPositioners")
 
-    local islandPositioners = islandPositionerFolder:GetChildren()
+    -- local islandPositioners = islandPositionerFolder:GetChildren()
 
     local islandTemplate = Utils.getFromTemplates("IslandTemplate")
     local sectorConfigs = {
