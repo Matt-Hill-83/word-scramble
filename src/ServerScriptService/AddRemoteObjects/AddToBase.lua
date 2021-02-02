d559a7129a07b535b1f0e0e4f54ff3a4a8390069
Matt Hill <matt.hill.email@gmail.com>
2021-02-02 09:15:31 -0800

    move SectorConfigs to new component

--- src/ServerScriptService/AddRemoteObjects/AddToBase.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase.lua
@@ -5,6 +5,7 @@ local CS = game:GetService("CollectionService")
 local Utils = require(Sss.Source.Utils.U001GeneralUtils)
 local Utils3 = require(Sss.Source.Utils.U003PartsUtils)
 local Constants = require(Sss.Source.Constants.Constants)
+local SectorConfigs = require(Sss.Source.SectorConfigs.SectorConfigs)
 
 local PlayerStatManager = require(Sss.Source.AddRemoteObjects.PlayerStatManager)
 local ConfigGame = require(Sss.Source.AddRemoteObjects.ConfigGame)
@@ -14,63 +15,63 @@ local Entrance = require(Sss.Source.BlockDash.Entrance)
 local function addRemoteObjects()
     local myStuff = workspace:FindFirstChild("MyStuff")
 
-    local sector1Config = {
-        words = {
-            -- "NAP", --
-            -- "TAP", --
-            -- "RAP", --
-            "ZAP" --
-        }
-    }
+    -- local sector1Config = {
+    --     words = {
+    --         -- "NAP", --
+    --         -- "TAP", --
+    --         -- "RAP", --
+    --         "ZAP" --
+    --     }
+    -- }
 
-    local sector2Config = {
-        words = {
-            "CAP", --
-            "GAP", --
-            "LAP", --
-            "MAP" --
-        }
-    }
+    -- local sector2Config = {
+    --     words = {
+    --         "CAP", --
+    --         "GAP", --
+    --         "LAP", --
+    --         "MAP" --
+    --     }
+    -- }
 
-    local sector3Config = {
-        words = {
-            "VAN", --
-            "RAN", --
-            "CAN", --
-            "AN" --
-        }
-    }
+    -- local sector3Config = {
+    --     words = {
+    --         "VAN", --
+    --         "RAN", --
+    --         "CAN", --
+    --         "AN" --
+    --     }
+    -- }
 
-    local sector4Config = {
-        words = {
-            "PAN", --
-            "DAN", --
-            "FAN", --
-            "TAN", --
-            "JAN" --
-        }
-    }
-    local sector5Config = {
-        words = {
-            "SAT", --
-            "RAT", --
-            "VAT", --
-            "AT" --
+    -- local sector4Config = {
+    --     words = {
+    --         "PAN", --
+    --         "DAN", --
+    --         "FAN", --
+    --         "TAN", --
+    --         "JAN" --
+    --     }
+    -- }
+    -- local sector5Config = {
+    --     words = {
+    --         "SAT", --
+    --         "RAT", --
+    --         "VAT", --
+    --         "AT" --
 
-        }
-    }
+    --     }
+    -- }
 
-    local sector6Config = {
-        -- words = {
-        --     "CAT" --
-        -- }
-        words = {
-            "CAT", --
-            "HAT", --
-            "MAT", --
-            "PAT" --
-        }
-    }
+    -- local sector6Config = {
+    --     -- words = {
+    --     --     "CAT" --
+    --     -- }
+    --     words = {
+    --         "CAT", --
+    --         "HAT", --
+    --         "MAT", --
+    --         "PAT" --
+    --     }
+    -- }
 
     local blockDash = Utils.getFirstDescendantByName(myStuff, "BlockDash")
     local mainSpawn = Utils.getFirstDescendantByName(myStuff, "MainSpawn")
@@ -78,14 +79,15 @@ local function addRemoteObjects()
 
     local islandPositioners = CS:GetTagged("IslandPositioner")
     local islandTemplate = Utils.getFromTemplates("IslandTemplate")
-    local sectorConfigs = {
-        sector1Config, --
-        sector2Config, --
-        sector3Config, --
-        sector4Config, --
-        sector5Config, --
-        sector6Config --
-    }
+    local sectorConfigs = SectorConfigs.sectorConfigs[1]
+    -- local sectorConfigs = {
+    --     sector1Config, --
+    --     sector2Config, --
+    --     sector3Config, --
+    --     sector4Config, --
+    --     sector5Config, --
+    --     sector6Config --
+    -- }
 
     local xxxsectorConfigs = {
         {
