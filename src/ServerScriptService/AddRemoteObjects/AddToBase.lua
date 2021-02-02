f93bcf947bd3974078d4ec6592fd01950dd63195
Matt Hill <matt.hill.email@gmail.com>
2021-02-02 11:20:34 -0800

    works

--- src/ServerScriptService/AddRemoteObjects/AddToBase.lua
+++ src/ServerScriptService/AddRemoteObjects/AddToBase.lua
@@ -18,61 +18,56 @@ local function addRemoteObjects()
     local blockDash = Utils.getFirstDescendantByName(myStuff, "BlockDash")
     local levelsFolder = Utils.getFirstDescendantByName(myStuff, "Levels")
     local levels = levelsFolder:GetChildren()
-    print('levels' .. ' - start');
-    print('levels' .. ' - start');
-    print('levels' .. ' - start');
-    print(levels);
-    Entrance.initEntrance(workspace)
 
     print('levels[1]' .. ' - start');
     print(levels[1]);
 
     Utils.sortListByObjectKey(levels, "Name")
-    local islandPositioners = Utils.getByTagInParent(
-                                  {parent = levels[1], tag = "IslandPositioner"})
 
-    print('islandPositioners' .. ' - start');
-    print(islandPositioners);
-
-    -- local islandPositioners = CS:GetTagged("IslandPositioner")
     local islandTemplate = Utils.getFromTemplates("IslandTemplate")
-    local sectorConfigs = LevelConfigs.levelConfigs[1]
 
-    Utils.sortListByObjectKey(islandPositioners, "Name")
+    for levelIndex, level in ipairs(levels) do
+        local islandPositioners = Utils.getByTagInParent(
+                                      {parent = level, tag = "IslandPositioner"})
 
-    local myPositioners = Constants.gameConfig.singleIsland and
-                              {islandPositioners[1]} or islandPositioners
+        local sectorConfigs = LevelConfigs.levelConfigs[1]
+        Utils.sortListByObjectKey(islandPositioners, "Name")
 
-    for islandIndex, islandPositioner in ipairs(myPositioners) do
+        local myPositioners = Constants.gameConfig.singleIsland and
+                                  {islandPositioners[1]} or islandPositioners
 
-        -- if islandIndex == 3 then break end
-        local newIsland = islandTemplate:Clone()
+        Entrance.initEntrance(level)
 
-        local anchoredParts = {}
-        for _, child in pairs(newIsland:GetDescendants()) do
-            if child:IsA("BasePart") then
-                if child.Anchored then
-                    child.Anchored = false
-                    table.insert(anchoredParts, child)
+        for islandIndex, islandPositioner in ipairs(myPositioners) do
+            -- if islandIndex == 3 then break end
+            local newIsland = islandTemplate:Clone()
+
+            local anchoredParts = {}
+            for _, child in pairs(newIsland:GetDescendants()) do
+                if child:IsA("BasePart") then
+                    if child.Anchored then
+                        child.Anchored = false
+                        table.insert(anchoredParts, child)
+                    end
                 end
             end
+
+            newIsland.Parent = myStuff
+            newIsland.Name = "Sector-" .. islandPositioner.Name
+            local newIslandPart = newIsland.PrimaryPart
+
+            local sectorConfig = sectorConfigs[(islandIndex % #sectorConfigs) +
+                                     1]
+            sectorConfig.sectorFolder = newIsland
+            sectorConfig.islandPositioner = islandPositioner
+
+            for _, child in pairs(anchoredParts) do
+                child.Anchored = true
+                -- 
+            end
+            BlockDash.addBlockDash(sectorConfig)
         end
-
-        newIsland.Parent = myStuff
-        newIsland.Name = "Sector-" .. islandPositioner.Name
-        local newIslandPart = newIsland.PrimaryPart
-
-        local sectorConfig = sectorConfigs[(islandIndex % #sectorConfigs) + 1]
-        sectorConfig.sectorFolder = newIsland
-        sectorConfig.islandPositioner = islandPositioner
-
-        for _, child in pairs(anchoredParts) do
-            child.Anchored = true
-            -- 
-        end
-        BlockDash.addBlockDash(sectorConfig)
     end
-
     -- Do this last after everything has been created/deleted
     ConfigGame.configGame()
     PlayerStatManager.init()
